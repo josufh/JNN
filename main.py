5789339ac0b726a6069d19ef9dd86db73615875a
Josu Ferrer <josuferrer@outlook.com>
2021-06-29 15:07:04 +0900

    Add NEURALNETWORK threshhold activation function and guess functionality, it outputs fine but may not work properly

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from neuralnetwork import NeuralNetwork
 
 nn = NeuralNetwork(2, 2, 1)
+
+inputs = [1, 0]
+
+guess = nn.guess(inputs)
+
+print(guess)
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -13,15 +13,38 @@ class NeuralNetwork():
 
         self.HW = Matrix(n_hidden, n_inputs+1)
         self.HW.rand(-1, 1)
-        print(self.HW)
 
         self.OW = Matrix(n_outputs, n_hidden+1)
         self.OW.rand(-1, 1)
-        print(self.OW)
 
     def guess(self, inputs):
-        pass
+        inputs += [1]
+        input_matrix = Matrix(self.n_i+1, 1)
+        input_matrix.initValues(inputs)
+        hidden = self.HW*input_matrix
+        hidden = NeuralNetwork.threshhold(hidden)
+        
+        hidden.matrix += [1]
+        hidden.n_rows += 1
+        output_matrix = self.OW*hidden
+        output_matrix = NeuralNetwork.threshhold(output_matrix)
+        return output_matrix
 
     @staticmethod
-    def sigmoid(x):
-        return 1/(1+pow(math.e, -x))
+    def sigmoid(matrix):
+        values = []
+        for m in matrix.matrix:
+            values += [1/(1+pow(math.e, -m))]
+        new_matrix = Matrix(matrix.n_rows, matrix.n_cols)
+        new_matrix.initValues(values)
+        return new_matrix
+
+    @staticmethod
+    def threshhold(matrix):
+        values = []
+        for m in matrix.matrix:
+            if m < 0: values += [0]
+            else: values += [1]
+        new_matrix = Matrix(matrix.n_rows, matrix.n_cols)
+        new_matrix.initValues(values)
+        return new_matrix
