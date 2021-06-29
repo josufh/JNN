2eb474d0f36ca85df54b2a353d18282aa9f7dce2
Josu Ferrer <josuferrer@outlook.com>
2021-06-29 11:00:16 +0900

    Matrix add overload

--- jmath.py
+++ jmath.py
@@ -3,6 +3,7 @@ class Matrix:
     def __init__(self, rows, cols):
         self.n_rows = rows
         self.n_cols = cols
+        self.n_elements = rows*cols
         self.matrix = []
 
         for i in range(0, rows*cols):
@@ -10,12 +11,23 @@ class Matrix:
 
     def initValues(self, matrix):
         length = len(matrix)
-        if length != self.n_rows*self.n_cols:
+        if length != self.n_elements:
             print("MATRIX error on initValues: matrix aren't the same size.")
             return
         
         self.matrix = matrix
-    
+
+    def __add__(self, v):
+        if isinstance(v, int) or isinstance(v, float):
+            for i in range(0, self.n_elements):
+                self.matrix[i] += v
+        if isinstance(v, Matrix):
+            for i in range(0, self.n_elements):
+                self.matrix[i] += v[i]
+        new_matrix = Matrix(self.n_rows, self.n_cols)
+        new_matrix.initValues(self.matrix)
+        return new_matrix
+
     def __str__(self):
         s = '\n' + str(self.n_rows) + 'x' + str(self.n_cols) + ' matrix\n'
         for i in range(0, self.n_rows):
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ nn = NeuralNetwork(2, 2, 1)
 
 m = Matrix(2, 3)
 
-matrix = [2, 3, 4, 5, 6]
+matrix = [1, 2, 3, 4, 5, 6]
 m.initValues(matrix)
+m += 1
+
 print(m)
