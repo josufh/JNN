335c2971096d4e96a08b9f53c9c4c52fbe8faff2
Josu Ferrer <josuferrer@outlook.com>
2021-06-29 11:30:54 +0900

    Matrix mul overload with scalar and rand

--- jmath.py
+++ jmath.py
@@ -1,3 +1,5 @@
+import random
+
 class Matrix:
 
     def __init__(self, rows, cols):
@@ -17,16 +19,37 @@ class Matrix:
         
         self.matrix = matrix
 
+    def rand(self, min=0, max=1):
+        for i in range(0, self.n_elements):
+            self.matrix[i] = random.uniform(min, max)
+
     def __add__(self, v):
         if isinstance(v, int) or isinstance(v, float):
             for i in range(0, self.n_elements):
                 self.matrix[i] += v
         if isinstance(v, Matrix):
+            if self.n_rows != v.n_rows or self.n_cols != v.n_cols:
+                print("MATRIX error on add: matrix aren't the same size.")
+                return self
             for i in range(0, self.n_elements):
-                self.matrix[i] += v[i]
+                self.matrix[i] += v.matrix[i]
+
         new_matrix = Matrix(self.n_rows, self.n_cols)
         new_matrix.initValues(self.matrix)
         return new_matrix
+
+    __radd__ = __add__
+
+    def __mul__(self, v):
+        if isinstance(v, int) or isinstance(v, float):
+            for i in range(0, self.n_elements):
+                self.matrix[i] *= v
+        
+        new_matrix = Matrix(self.n_rows, self.n_cols)
+        new_matrix.initValues(self.matrix)
+        return new_matrix
+
+    __rmul__ = __mul__
 
     def __str__(self):
         s = '\n' + str(self.n_rows) + 'x' + str(self.n_cols) + ' matrix\n'
--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from jmath import Matrix
 nn = NeuralNetwork(2, 2, 1)
 
 m = Matrix(2, 3)
+n = Matrix(2, 3)
 
 matrix = [1, 2, 3, 4, 5, 6]
+natrix = [1, 2, 3, 4, 5, 6]
+
 m.initValues(matrix)
-m += 1
+n.initValues(natrix)
+
+m = 2*m
 
 print(m)
