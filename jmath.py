1470dd159b9296ba7c0bd09b1cad9cf75846aefd
Josu Ferrer <josuferrer@outlook.com>
2021-06-29 10:41:47 +0900

    Added Matrix init, str and initValues

--- jmath.py
+++ jmath.py
@@ -1,2 +1,27 @@
-class JMath:
-    pass
+class Matrix:
+
+    def __init__(self, rows, cols):
+        self.n_rows = rows
+        self.n_cols = cols
+        self.matrix = []
+
+        for i in range(0, rows*cols):
+            self.matrix += [0]
+
+    def initValues(self, matrix):
+        length = len(matrix)
+        if length != self.n_rows*self.n_cols:
+            print("MATRIX error on initValues: matrix aren't the same size.")
+            return
+        
+        self.matrix = matrix
+    
+    def __str__(self):
+        s = '\n' + str(self.n_rows) + 'x' + str(self.n_cols) + ' matrix\n'
+        for i in range(0, self.n_rows):
+            s += '  '
+            for j in range(0, self.n_cols):
+                s += str(self.matrix[j + i*self.n_cols])
+                s += ' '
+            s += '\n'
+        return s
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from neuralnetwork import NeuralNetwork
+from jmath import Matrix
 
 nn = NeuralNetwork(2, 2, 1)
+
+m = Matrix(2, 3)
+
+matrix = [2, 3, 4, 5, 6]
+m.initValues(matrix)
+print(m)
